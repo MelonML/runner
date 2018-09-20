21c6e7085f2c6e9aab9ade0df1bf05e4c0c560a0
Soeren Wegener <wegener92@gmail.com>
2018-09-21 01:10:53 +0200

    Test with lower number of iterations in final_fit, save the trained model

--- src/app.py
+++ src/app.py
@@ -14,10 +14,14 @@ def main():
     })
     print(clf)
     # clf.fit(x_train, y_train, time_limit=12 * 60 * 60)
-    clf.fit(x_train, y_train, time_limit=30 * 60)
-    clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
+    clf.fit(x_train, y_train, time_limit=10 * 60)
+    # clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
+    clf.final_fit(x_train, y_train, x_test, y_test, trainer_args={
+        'max_iter_num': 3,
+    })
     y = clf.evaluate(x_test, y_test)
     print(y)
+    clf.load_searcher().load_best_model().produce_keras_model().save('my_model.h5')
 
 
 if __name__ == '__main__':
