b42a3921b43eccd1a7b522826e19290535b07f5e
Soeren Wegener <wegener92@gmail.com>
2018-09-21 18:16:13 +0200

    Print total training time

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
 from keras.datasets import mnist
 from autokeras.image_supervised import ImageClassifier
-
+from datetime import datetime
 
 def main():
+    started = datetime.now()
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = x_train.reshape(x_train.shape + (1,))
     x_test = x_test.reshape(x_test.shape + (1,))
@@ -22,6 +23,8 @@ def main():
     y = clf.evaluate(x_test, y_test)
     print(y)
     clf.load_searcher().load_best_model().produce_keras_model().save('my_model.h5')
+    finished = datetime.now() - started
+    print(f'Total training duration: {finished}')
 
 
 if __name__ == '__main__':
